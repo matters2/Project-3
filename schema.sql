0c7f48118fc8d27ea868510160d5120d0b864dd4
Jeremy <jeremy.brockhoff@gmail.com>
2020-05-18 16:54:31 +1000

    inital commit

---
+++ schema.sql
@@ -0,0 +1,32 @@
+Create database myPet;
+
+create table users (
+  id serial primary key,
+  username text,
+  email text,
+  password_digest text
+);
+
+create table pets (
+  id serial primary key,
+  user_id integer,
+  species text,
+  dob date,
+  name text,
+  image_url text
+);
+
+create table appointments (
+  id serial primary key,
+  pet_id integer,
+  appt_type text,
+  location text,
+  appt_date date,
+  comments text
+);
+
+create table meds (
+  id serial primary key,
+  pet_id integer,
+  comments text
+);
