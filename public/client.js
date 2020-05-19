9bb9453971e99486bd008edd2dc729c4333ff7f9
Mirko <mirkomujica@yahoo.com.au>
2020-05-19 11:59:27 +1000

    added new user form and new pet form

--- public/client.js
+++ public/client.js
@@ -18,3 +18,27 @@ btnNewUser.addEventListener('click', e => {
     })
 
 })
+
+
+
+
+const btnNewPet = document.querySelector('.btnNewPet')
+
+btnNewPet.addEventListener('click', e => {
+    e.preventDefault()
+
+    var params = {
+        userId: document.querySelector('.userId').value,
+        species: document.querySelector('.species').value,
+        dob: document.querySelector('.dob').value,
+        name: document.querySelector('.name').value,
+        image_url: document.querySelector('.image_url').value
+    }
+
+    const url = 'http://localhost:8080/api/pets/new'
+    
+    axios.post(url, params).then(res => {
+        console.log('bug')
+    })
+
+})
