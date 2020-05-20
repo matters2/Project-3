8ee0ebbd1cb384c621c9ffce47b3c21d5732868d
Jeremy <jeremy.brockhoff@gmail.com>
2020-05-20 13:31:52 +1000

    got sessions working

--- models/func_sessions.js
+++
@@ -1,4 +0,0 @@
-const db = require("./db/config")
-const pwd = require("./models/password")
-
-
--- server.js
+++ server.js
@@ -4,35 +4,41 @@ const express = require("express")
 const port = 8080
 const db = require("./db/config")
 const pwd = require("./models/password")
+const userdb = require("./models/func_sessions")
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const petController = require('./controllers/pet');
 const userController = require('./controllers/user');
 const apptController = require('./controllers/appointments');
 const medsController = require('./controllers/meds');
-// const passport = require('passport');
-// const Strategy = require('passport-local').Strategy;
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
 
 
 
-// passport.use(new Strategy((username, password, cb) => {
-//       callbensfucntionforuser(username, (err, user) => {
-//         if (!user) { return cb(null, false); }
-//         if (user.password != password) { return cb(null, false); }
-//         return cb(null, user);
-//       });
-// }));
+passport.use(new Strategy((username, password, cb) => {
+    db.query(
+        "select * from users where username = $1",
+        [username],
+        (err, dbRes) => {
+          return cb(null, dbRes.rows[0])
+        }
+    )
+}));
 
-// passport.serializeUser(function(user, cb) {
-//     cb(null, user.id);
-// });
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
   
-// passport.deserializeUser((id, cb) => {
-//     db.users.findById(id, function (err, user) {
-//         if (err) { return cb(err); }
-//         cb(null, user);
-//     });
-// });
+passport.deserializeUser((id, cb) => {
+    db.query(
+        "select * from users where id = $1",
+        [id],
+        (err, dbRes) => {
+          return cb(null, dbRes.rows[0])
+        }
+    )
+});
 
 
 
@@ -44,72 +50,61 @@ const app = express()
 app.use(morgan("combined"))
 
 app.use(bodyParser.json()) // reads params from body
+app.use(bodyParser.urlencoded({extended: true})) 
 
-app.use(express.static('public'))
 
-// app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 ////////// MIDDLEWARE ///////////
 
+app.set('view engine', 'ejs')
+
+// app.get("/", (req,res) => { // this is to look at the user session stuff
+//     res.json(req.user)
+// })
+app.use(express.static('public'))
 app.use('/', petController)
 app.use('/', userController)
 app.use('/', apptController)
 app.use('/', medsController)
 
-app.get("/", (req,res) => {
-    res.send("hello world")
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/login/new', (req, res) => {
+    res.render('login-new');
+});
+
+app.post('/login/new', (req, res) => {
+
+    passwordDigest = pwd.digest(req.body.password)
+    db.query(
+        "INSERT INTO users (username, email, password_digest) VALUES ($1, $2, $3);",
+        [req.body.username, req.body.email, passwordDigest],
+        (err, dbRes) => {
+            res.redirect('/login')
+        }
+    )
 })
 
-// app.get('/login', (req, res) => {
-//     res.render('login');
-// });
+app.post('/login', 
+    passport.authenticate(
+        'local',
+        { failureRedirect: '/login' }
+    ),
+    (req, res) => {
+        res.redirect('/');
+});
 
-// app.post('/login', 
-//     passport.authenticate('local', { failureRedirect: '/login' }),
-//     function(req, res) {
-//         res.redirect('/');
-//     });
-
-// app.get('/logout', (req, res) => {
-//     req.logout();
-//     res.redirect('/');
-//   });
-
-
-
-
-
-
-// app.get("/pet", (req, res) => {
-//     ​
-//     // let dbPets = [
-//     //     {
-//     //     name: "Poochie",
-//     //     species: "dog",
-//     //     dob: "2020-05-13",
-//     //     image_url: "dog.jpg"
-//     //     },
-//     //     {
-//     //     name: "Puss n Boots",
-//     //     species: "cat",
-//     //     dob: "2019-10-09",
-//     //     image_url: "cat.jpg"
-//     //     },
-//     //     {
-//     //     name: "Smoothy",
-//     //     species: "possum",
-//     //     dob: "2018-02-28",
-//     //     image_url: "possum.jpg"
-//     //     }
-//     // ]
-//     ​
-//     ​
-//     ​
-//     // res.render('dashboard', { pets: dbPets })
-// }) 
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+});
 
 
 
